d16076114b8cd3b622c6ea477d73cb8c304330df
A645095 <swapnil.barde07@rediffmail.com>
2024-09-02 21:04:39 +0530

    Spring Boot 4

---
+++ EventRegistration II Template/src/main/java/com/example/EventRegistration/MyCollege.java
@@ -0,0 +1,38 @@
+package com.example.EventRegistration;
+
+public class MyCollege implements College {
+    String name="Vivekanand College";
+    CollegeEvent event;
+
+
+    public void setEvent(CollegeEvent event) {
+        this.event = event;
+    }
+
+    @Override
+    public String getCollegeName() {
+        return this.name;
+    }
+
+    @Override
+    public CollegeEvent getEvent() {
+        return this.event;
+    }
+
+
+    /*
+        1. Create attribute name (string) and initialize it with some default name.
+
+        2. Inject the CollegeEvent dependency using setter injection as shown below:
+
+                public void setEvent(CollegeEvent event) {
+                        this.event = event;
+                }
+
+        Note: You have to create "CollegeEvent event" object to be able to inject it using
+              setEvent method.
+
+        3. Make the class College interface implementation and override the interface methods.
+     */
+
+}
